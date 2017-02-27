52fa2ad382c97a6ca400ad7ceace8d0bdfbc31bc
Kirill Prostyakov <kprostyakov@gmail.com>
2017-02-27 04:09:06 +0300

    works, need to fix points order

--- src/svg2csv.py
+++ src/svg2csv.py
@@ -2,7 +2,8 @@
 from sys import argv
 from json import load
 import xml.etree.ElementTree as ET
-from re import search
+from re import findall
+import csv
 
 NS = {
     'svg': 'http://www.w3.org/2000/svg'
@@ -36,29 +37,71 @@ def collect_siblings(parent, id_from, id_to):
 
 def parse_circles(element):
     'Dumps translate attributes in subtree'
+    def get_shift(el):
+        'Extracts shift vector from transform string'
+        return tuple(float(x) for x in findall(r'(-?\d+\.?\d*)', el.attrib["transform"]))
+    def sum_tuples(a, b):
+        'Element-wise tuple sum'
+        return tuple(sum(x) for x in zip(a, b))
+    position = [get_shift(element)]
     for child in element.findall('.//svg:g', NS):
         try:
-            vector = search(r'\((\S+)\)', child.attrib["transform"]).group(1).split(',')
-            print(child.attrib["id"], vector)
+            shift = sum_tuples(position[-1], get_shift(child))
+            position.append(shift)
         except KeyError:
             continue
-    return None
+    return position
+
+def parse_crosses():
+    'Dumps translate attributes skipping every 4'
+
+def make_scale_func(coeff):
+    'Closure for fixed params of scaling function'
+    def scale(val, c):
+        slope = (c['max_u']-c['min_u'])/(c['max_s']-c['min_s'])
+        return c['min_u']+(val+c['off']-c['min_s'])*slope
+    def scale_func(value):
+        nonlocal coeff
+        return tuple(scale(value[i], coeff[i]) for i in range(2))
+    return scale_func
 
 def parse_helper(config_file_name):
     'parses input SVG file'
     parsers = {
-        "circles": parse_circles
+        "circles": parse_circles,
+        "crosses": parse_crosses
     }
     with open(config_file_name) as json_data:
         task_config = load(json_data)
     for task in task_config:
         tree = ET.parse(task['input_file'])
         root = tree.getroot()
-        for graph in task["graph"]:
-            container = find_start_elem(root, graph["id_from"])
-            parse_ids = collect_siblings(container, graph["id_from"], graph["id_to"])
+        for series in task["series"]:
+            container = find_start_elem(root, series["id_from"])
+            parse_ids = collect_siblings(container, series["id_from"], series["id_to"])
+            data = []
             for elem_id in parse_ids:
-                parsers[graph["type"]](container.find('./*[@id="{}"]'.format(elem_id)))
+                data += parsers[series["type"]](container.find('./*[@id="{}"]'.format(elem_id)))
+            coeff = ({
+                'off': series["elem_offset_svg"][0],
+                'min_u': task["x_range_shown"][0],
+                'max_u': task["x_range_shown"][1],
+                'min_s': task["x_range_svg"][0],
+                'max_s': task["x_range_svg"][1]
+            }, {
+                'off': series["elem_offset_svg"][1],
+                'min_u': series["y_range_shown"][0],
+                'max_u': series["y_range_shown"][1],
+                'min_s': series["y_range_svg"][0],
+                'max_s': series["y_range_svg"][1]
+            })
+            scale_func = make_scale_func(coeff)
+            scaled_data = [scale_func(item) for item in
+                           sorted(data, key=lambda item: item[0])]
+            with open(task['output_prefix']+series['y_name']+'.csv', 'wt') as fout:
+                print('{},{}'.format(task['x_name'],series['y_name']), file=fout)
+                cout = csv.writer(fout)
+                cout.writerows(scaled_data)
 
 if __name__ == '__main__':
     parse_helper(argv[1])
